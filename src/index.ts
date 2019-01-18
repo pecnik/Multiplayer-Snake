90ecf6eb2f2d8b64416068257b70ca67031c0f5e
Blaž Pečnik <blaz@easistent.com>
2019-01-18 18:36:53 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import { Game } from "./snake/Game";
+
+{
+    "use strict";
+
+    const game = new Game();
+
+    const cellSize = 8;
+    const width = game.cols * cellSize;
+    const height = game.rows * cellSize;
+
+    const app = document.getElementById("app");
+    if (app === null) {
+        throw new Error("Missing #app element");
+    }
+
+    const cavnas = document.createElement("canvas");
+    const buffer = document.createElement("canvas");
+    cavnas.width = buffer.width = width;
+    cavnas.height = buffer.height = height;
+    app.appendChild(cavnas);
+
+    setInterval(() => {
+        game.update();
+        requestAnimationFrame(render);
+    }, 1000 / 2);
+
+    function render() {
+        const ctx = buffer.getContext("2d");
+        if (ctx === null) return;
+
+        ctx.clearRect(0, 0, width, height);
+
+        ctx.fillStyle = "#f2f2f2";
+        game.snake.cells.forEach(cell => {
+            ctx.fillRect(
+                cell.x * cellSize,
+                cell.y * cellSize,
+                cellSize,
+                cellSize
+            );
+        });
+
+        const screen = cavnas.getContext("2d");
+        if (screen !== null) {
+            screen.clearRect(0, 0, width, height);
+            screen.drawImage(buffer, 0, 0);
+        }
+    }
+}
---
+++ src/snake/Game.ts
@@ -0,0 +1,69 @@
+export class Game {
+    public rows = 64;
+    public cols = 64;
+
+    public snake = this.createSnake();
+
+    public createSnake() {
+        const length = 8;
+        const x = Math.floor(this.cols * 0.5) - Math.floor(length * 0.5);
+        const y = Math.floor(this.rows * 0.5);
+
+        const snake = new Snake();
+        for (let i = 0; i < length; i++) {
+            snake.cells.push(new Cell(x - i, y));
+        }
+
+        return snake;
+    }
+
+    public advanceSnake(snake: Snake) {
+        const head = snake.cells.pop();
+        if (head !== undefined) {
+            head.x = snake.cells[0].x;
+            head.y = snake.cells[0].y;
+
+            switch (snake.direction) {
+                case Direction.up:
+                    head.y--;
+                    break;
+                case Direction.down:
+                    head.y++;
+                    break;
+                case Direction.left:
+                    head.x--;
+                    break;
+                case Direction.right:
+                    head.x++;
+                    break;
+            }
+
+            snake.cells.unshift(head);
+        }
+    }
+
+    public update() {
+        this.advanceSnake(this.snake);
+    }
+}
+
+export class Snake {
+    public direction = Direction.right;
+    public cells = new Array<Cell>();
+}
+
+export class Cell {
+    public x: number;
+    public y: number;
+    public constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+export enum Direction {
+    up,
+    down,
+    left,
+    right
+}
