7169125d5b04fbbcf73c48518dd582b142284bb8
Blaž Pečnik <blaz@easistent.com>
2019-01-19 11:25:33 +0100

    feat: besic server setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const PORT = 8080;
+
+const app = express();
+app.use("/", express.static(__dirname + "/dist"));
+// app.use("/assets", express.static(__dirname + "/assets"));
+
+// use  bundler
+const env = process.env.NODE_ENV.trim();
+if (env === "development") {
+    const path = require("path");
+    const Bundler = require("parcel-bundler");
+    const entryFile = path.join(__dirname, "./src/index.html");
+
+    // @ts-ignore
+    const bundler = new Bundler(entryFile, { hmr: false });
+
+    // @ts-ignore
+    app.use(bundler.middleware());
+}
+
+// run game server
+const srv = app.listen(PORT);
+const io = require("socket.io").listen(srv);
