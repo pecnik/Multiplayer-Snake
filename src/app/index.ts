2df02b89a3232b1d9ec67d60dd72d83b8155fe02
Blaž Pečnik <blaz@easistent.com>
2019-01-20 12:59:52 +0100

    feat: wip - player name validation

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,12 +1,70 @@
 import { GameClient } from "../game/GameClient";
+import { getPlayerNameErrors } from "../game/Selectors";
+import { debounce } from "lodash";
 
-{
-    "use strict";
+getPlayerName().then(startGame);
 
-    const $el = document.getElementById("app");
-    if ($el === null) {
+function getPlayerName() {
+    return new Promise(resolve => {
+        const $input = document.querySelector("#input") as HTMLInputElement;
+        const $error = document.querySelector("#error") as HTMLElement;
+        const $btn = document.querySelector("#login-btn") as HTMLButtonElement;
+        if (!$error || !$input || !$btn) {
+            throw new Error("Missing #login element");
+        }
+
+        const updateErrorMsg = debounce((error?: string | undefined) => {
+            console.log({ error });
+            if (error !== undefined) {
+                $error.style.display = "block";
+                $error.innerHTML = error;
+            } else {
+                $error.style.display = "none";
+            }
+        }, 100);
+
+        const tryLogin = () => {
+            const name = $input.value;
+            const error = getPlayerNameErrors(name);
+            if (error) {
+                console.log("TRY", error);
+                $btn.disabled = true;
+                updateErrorMsg(error);
+                return;
+            }
+
+            console.log("OK", { name });
+        };
+
+        $btn.addEventListener("click", tryLogin);
+
+        $input.addEventListener(
+            "keydown",
+            debounce(ev => {
+                if (ev.keyCode === 13) {
+                    tryLogin();
+                    return;
+                }
+
+                const value = $input.value || "";
+                const error = getPlayerNameErrors($input.value);
+                $btn.disabled = error !== undefined;
+
+                if (value.length >= 3 && error) {
+                    updateErrorMsg(error);
+                } else {
+                    updateErrorMsg();
+                }
+            })
+        );
+    });
+}
+
+function startGame() {
+    const $app = document.getElementById("app");
+    if ($app === null) {
         throw new Error("Missing #app element");
     }
 
-    GameClient($el);
+    GameClient($app);
 }
